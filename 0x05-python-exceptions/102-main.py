b30d35df8b44a88e498e6076c5756e7c07f64420
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-19 00:32:52 +0100

    Update 102-main.py

--- 0x05-python-exceptions/102-main.py
+++ 0x05-python-exceptions/102-main.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-from dis import dis
+safe_print_list_integers = \
+    __import__('2-safe_print_list_integers').safe_print_list_integers
 
-m_calc = __import__('102-magic_calculation').magic_calculation
-dis(m_calc)
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
