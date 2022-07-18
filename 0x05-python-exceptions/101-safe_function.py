8f8fbb54cdc54266c14d147e01177ed671498cff
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-18 09:17:49 +0100

    Create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    """Executes a function safely."""
+
+    try:
+        result = fct(*args)
+        return result
+    except Exception as exc:
+        print("Exception: {}".format(exc.args[0]), file=stderr)
+        return None
