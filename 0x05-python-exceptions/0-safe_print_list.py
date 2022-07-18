e7084db1273512349d2a4b0d0f0e81fcecd25838
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-18 08:20:13 +0100

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    """Prints x elements of a list."""
+
+    res = 0
+    for i in range(0, x):
+        try:
+            print(my_list[i], end="")
+            res += 1
+        except:
+            break
+
+    print("")
+
+    return res
