66ffc5f6b5a54729a5bbaf9fd3a8e6dbf40c9c9f
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-19 20:04:15 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        try:
+        except Exception:
             result = b + a
             break
     return result
