8601c79d110bbb521ee4d3d1164702b7ef923e87
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-26 22:26:06 +0100

    Update 100-main.py

--- 0x09-python-everything_is_object/100-main.py
+++ 0x09-python-everything_is_object/100-main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(B=[]):
-    B += ["BestSchool"]
-    return (", ".join(B))
+def magic_string(x=[0]):
+    x[0] = x[0] + 1
+    return (("BestSchool".join(" ,") * x[0])[:-1])
