21157568fef7acebbb681894142b6ec8f3ce9ca3
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-27 08:48:26 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 """
-This is the "5-test_indentation" module.
-
-The 5-text_indentation module supplies one function, text_indentation(text).
+Text_indentation Module
+adds two new lines after "?.:"
+doesn't print any spaces at the beginning or end of the sentences.
 """
 
 
 def text_indentation(text):
-"""splits a text into lines along "?", ":", "." followed by 2 new lines"""
-    if type(text) is not str:
+    """
+    text_indentation function:
+    checks to see if input is valid
+    adds two new lines after any instances of `?` or `.` or `:`
+    then prints the result without any new lines at the beginning
+    """
+    if text is None or not isinstance(text, str) or len(text) < 0:
         raise TypeError("text must be a string")
-    flag = 0
-    for a in text:
-        if flag == 0:
-            if a == ' ':
-                continue
-            else:
-           flag = 1
-        if flag == 1:
-            if a == '?' or a == '.' or a == ':':
-                print(a)
-                print()
-                flag = 0
-            else:
-                print(a, end="")
+    n = "".join([c if c not in "?.:" else c + "\n\n" for c in text])
+    print("\n".join([line.strip() for line in n.split("\n")]), end="")
