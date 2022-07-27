c032f6f754b5f96db01a40f50acfeb0141549175
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-27 09:51:07 +0100

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,8 @@
-def mutate_string(string, position, character):
-    slist = list(string)
-    slist[position] = character
-    return ''.join(slist)
+
+void print_python_string(PyObject *p);
+	if len(fourcopiesstringend) <= 1:
+		print("String must be two characters or greater.")
+	else:
+		print(fourcopiesstringend[len(fourcopiesstringend)-2:]*4)
+insertend("Python") #print onononon
+insertend("Exercises") #print eseseses
