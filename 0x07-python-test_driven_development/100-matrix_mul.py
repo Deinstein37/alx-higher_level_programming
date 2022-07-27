0996e152cd126fea4e9aff7ab00b1bf89b093735
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-27 09:53:47 +0100

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -3,6 +3,11 @@
 Contains the matrix_mul function
 """
 
+Write a function that prints My name is <first name> <last name>
+
+    Prototype: def matrix_mul(first_name, last_name=""):
+    first_name and last_name must be strings otherwise, raise a TypeError exception with the message first_name must be a string or last_name must be a string
+    You are not allowed to import any module
 
 def matrix_mul(m_a, m_b):
     """Multiply two matrices(lists of lists of integers/floats)"""
