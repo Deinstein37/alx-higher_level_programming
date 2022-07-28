99b14d57616fbb4ec44ad46f5a68ab4b4fdcad4a
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-28 19:17:15 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-"""
-This module defines `text_indentation`
+""" Doc """
+text_indentation = __import__('5-text_indentation').text_indentation
 
-The function prints a text with 2 new lines after each of
-these characters: ., ? and :
-"""
-
-def text_indentation(text):
-    """adds paragraph after `.`, `:` and `?`
-
-    Args:
-        text (str)
-    """
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
-
-    for l in '.:?':
-        text = text.replace(l, '{}\n'.format(l))
-    lines = text.splitlines()
-    for index, line in enumerate(lines):
-        print(line.strip(), end='' if index == len(lines) - 1 else '\n\n')
+try:
+    text_indentation(12)
+except Exception as e:
+    print(e)
