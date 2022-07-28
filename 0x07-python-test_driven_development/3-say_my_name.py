4aebb445c843840ddd04187f6520a237e4829f31
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-07-28 19:12:19 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python3
-"""
-This module defines `say_my_name`
+""" Doc """
+text_indentation = __import__('5-text_indentation').text_indentation
 
-The function prints the full name using first_name and last_name
-"""
-
-
-def say_my_name(first_name, last_name=""):
-    """prints a name using first_name and last_name
-
-    Args:
-        first_name (str)
-        last_name (str, optional). Defaults to "".
-
-    Raises:
-        TypeError: first_name must be a string
-        TypeError: last_name must be a string
-    """
-
-    if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
-    elif not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-
-    print('My name is {} {}'.format(first_name, last_name))
+try:
+    text_indentation(12)
+except Exception as e:
+    print(e)
