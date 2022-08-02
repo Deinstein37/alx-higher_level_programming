4cd959e4660b770ac681928d57f53b8c8eb874d8
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-08-02 14:21:22 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""read file"""
+
+
 def read_file(filename=""):
-    '''
-        Reads the contents of a file
-    '''
+    """read file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
