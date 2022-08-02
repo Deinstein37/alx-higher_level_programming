6c4ad3e615daa69be623e8f164a467624ca30a17
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-08-02 21:33:14 +0100

    Update 7-main.py

--- 0x0B-python-input_output/main file/7-main.py
+++ 0x0B-python-input_output/main file/7-main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+add_item = __import__('7-add_item').save_to_json_file
 
 filename = "my_list.json"
 my_list = [1, 2, 3]
-save_to_json_file(my_list, filename)
+add_item(my_list, filename)
 
 filename = "my_dict.json"
 my_dict = {
@@ -16,11 +16,11 @@ my_dict = {
         'average': 3.14
     }
 }
-save_to_json_file(my_dict, filename)
+add_item(my_dict, filename)
 
 try:
     filename = "my_set.json"
     my_set = { 132, 3 }
-    save_to_json_file(my_set, filename)
+    add_item(my_set, filename)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
