4bd5314b5162dd83b5468b9e727763923211275e
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-08-02 14:38:04 +0100

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-'''
-    Defines a student class
-'''
+""" Class Student """
 
 
 class Student:
-    '''
-        Defines a student class
-    '''
+    """ Method: __init__, to_json """
     def __init__(self, first_name, last_name, age):
-        '''
-            Initializing instance variables
-        '''
+        """ instantiate first_name, last_name, age """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        '''
-            Retrives the dict representation of the class
-        '''
-        return (self.__dict__)
+        """ Retrieves a dictionary representation of a Student instance """
+        return self.__dict__
