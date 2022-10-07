96c05684198f07b2fe0fd69d365c54a282ef3570
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-10-07 12:02:34 +0100

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
-
-import sqlalchemy
+import sys
+from relationship_state import State, Base
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
-from sys import argv
-
 
 if __name__ == "__main__":
-    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+    connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    user_name = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine(connection.format(user_name, password, db_name))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    rows = session.query(State).all()
-    for state in rows:
-        print("{}: {}".format(state.id, state.name))
-        for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
-    session.close()
+    query = session.query(State).order_by(State.id).all()
+    for row in query:
+        print("{}: {}".format(row.id, row.name))
+        for related_row in row.cities:
+            print("\t{}: {}".format(related_row.id, related_row.name))
