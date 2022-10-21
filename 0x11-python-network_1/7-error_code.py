9c10a094aacb34b704b678a0b308007639843bae
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-10-21 12:52:39 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     import requests
     from sys import argv
     r = requests.get(argv[1])
-    if r.status_code >= 500:
+    if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
