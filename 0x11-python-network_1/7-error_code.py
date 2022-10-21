ecf3c7b1619ae44aafa22273965948d28f54551e
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-10-21 19:15:54 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""
-takes in a URL, sends a request to the URL & displays the body of the response
-"""
-if __name__ == '__main__':
-    import requests
-    from sys import argv
-    r = requests.get(argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+''' url requests, added error handling  '''
+import requests
+from sys import argv
 
+
+if __name__ == "__main__":
+    reqst = requests.get(argv[1])
+    if reqst.status_code >= 400:
+        print("Error code: {}".format(reqst.status_code))
+    else:
+        print(reqst.text)
