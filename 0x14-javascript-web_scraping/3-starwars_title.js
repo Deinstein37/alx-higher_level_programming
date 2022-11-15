763375347c25004f40fa5c58829360bb16c13230
Onolameh Daniel <101498178+Deinstein37@users.noreply.github.com>
2022-11-15 10:50:31 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 
-
-
-const url = process.argv[2];
-
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 const request = require('request');
 
-
-
-request.get(url).on('response', (response) => {
-
-	  console.log(`code: ${response.statusCode}`);
-
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body.title);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
 });
